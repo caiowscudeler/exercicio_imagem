15be02a2c2cd55f2e34315ae210a4d2ca8a57f59
caiowscudeler <103973597+caiowscudeler@users.noreply.github.com>
2022-06-13 19:33:55 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class ExercicioDesenho {
+public class Helloworld {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		
-		int i, j, m[][] = new int[16][16];//declarar variavel de matriz 16x16
+		int n1, n2, m[][] = new int[16][16];//declarar variavel de matriz 16x16
 		
 		System.out.printf("Crie um desenho a partir de uma matriz 16x16\n");//exibir texto ao usuario
 	
-		for (i = 0; i < 16; i++) {//estrutura de repeticao
+		for (n1 = 0; n1 < 16; n1++) {//estrutura de repeticao
 
 			System.out.printf("Cores: Branco(0); Preto(1); Vermelho(2); e Bege(3).\nInforme as cores da %da. linha:\n",
-					(i + 1));//informacoes exibidas ao usuario e a contagem
-			for (j = 0; j < 16; j++) {//estrutura de repeticao com colunas da matriz
+					(n1 + 1));//informacoes exibidas ao usuario e a contagem
+			for (n2 = 0; n2 16; n2) {//estrutura de repeticao com colunas da matriz
 				System.out.printf("m[%d][%d] = ", i, j);//receber coordenada da matriz
 				m[i][j] = ler.nextInt();//guardar valor em coordenada
 			}
