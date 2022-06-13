ea8f3cfa7e1c1c427a66a616478ef8b156d87f84
caiowscudeler <103973597+caiowscudeler@users.noreply.github.com>
2022-06-13 19:38:39 -0300

    Update main.java

--- main.java
+++ main.java
@@ -13,10 +13,10 @@ public class Helloworld {
 
 			System.out.printf("Cores: Branco(0); Preto(1); Vermelho(2); e Bege(3).\nInforme as cores da %da. linha:\n",
 					(n1 + 1));//informacoes exibidas ao usuario e a contagem
-			for (n2 = 0; n2 < 16; n2) {//estrutura de repeticao com colunas da matriz
+			for (n2 = 0; n2 < 16; n2++) {//estrutura de repeticao com colunas da matriz
 				System.out.printf("m[%d][%d] = ", i, j);//receber coordenada da matriz
 				m[n1][n2] = ler.nextInt();//guardar valor em coordenada
-			}
+			
 			System.out.printf("\n");//pular linha
 		}
 		System.out.printf("\n");//pular linha
@@ -24,7 +24,7 @@ public class Helloworld {
 		for (n1 = 0; n1 < 16; n1++) {//estrutura de repeticao com linhas da matriz
 			for (n2 = 0; n2 < 16; n2++) {//estrutura de repeticao de colunas
 				System.out.printf("%d ", m[i][j]);//valor de acordo com as coordenadas
-			}
+			
 			System.out.printf("\n");//pular linha
 		}
 	}
