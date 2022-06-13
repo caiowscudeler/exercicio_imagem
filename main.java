0fb13e7cececec03d0ad8e637986102b2af2987d
caiowscudeler <103973597+caiowscudeler@users.noreply.github.com>
2022-06-13 19:50:38 -0300

    Update main.java

--- main.java
+++ main.java
@@ -5,27 +5,25 @@ public class Helloworld {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		
-		int n1, n2, m[][] = new int[16][16];//declarar variavel de matriz 16x16
+		int n, n1, m[][] = new int[16][16];//declarar variaveis da matriz
 		
-		System.out.printf("Crie um desenho a partir de uma matriz 16x16\n");//exibir texto ao usuario
-	
-		for (n1 = 0; n1 < 16; n1++) {//estrutura de repeticao
-
+		System.out.printf("Crie um desenho a partir de uma matriz 16x16\n");// texto exibido ao usuario
+		
+		for (n = 0; n < 16; n++) {//estrutura de repeticao
 			System.out.printf("Cores: Branco(0); Preto(1); Vermelho(2); e Bege(3).\nInforme as cores da %da. linha:\n",
-					(n1 + 1));//informacoes exibidas ao usuario e a contagem
-			for (n2 = 0; n2 < 16; n2++) {//estrutura de repeticao com colunas da matriz
-				System.out.printf("m[%d][%d] = ", i, j);//receber coordenada da matriz
-				m[n1][n2] = ler.nextInt();//guardar valor em coordenada
-			
-			System.out.printf("\n");//pular linha
+					(n + 1)); //texto exibido ao usuario co  a contagem
+			for (n1 = 0; n1 < 16; n1++) {//estrutura de repeticao
+				System.out.printf("m[%d][%d] = ", n, n1); //receber coordeenada da matriz do usuario
+				m[n][n1] = ler.nextInt();//guardar valor na matriz
+			}
+			System.out.printf("\n");// Pular linha
 		}
-		System.out.printf("\n");//pular linha
-	
-		for (n1 = 0; n1 < 16; n1++) {//estrutura de repeticao com linhas da matriz
-			for (n2 = 0; n2 < 16; n2++) {//estrutura de repeticao de colunas
-				System.out.printf("%d ", m[i][j]);//valor de acordo com as coordenadas
-			
-			System.out.printf("\n");//pular linha
+		System.out.printf("\n");// Pular linha
+		for (n = 0; n < 16; n++) {//estrutura de repticao por linhas
+			for (n1 = 0; n1 < 16; n1++) {//estrutura de repeticao por colunas
+				System.out.printf("%d ", m[n][n1]);//exibir valores da coordenada da matriz
+			}
+			System.out.printf("\n");// Pular linha 
 		}
 	}
 }
